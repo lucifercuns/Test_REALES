fd5896ed409a06eda1d0172663c112b3771d4364
TuanDoan <1512632@student.hcmus.edu.vn>
2019-07-15 17:14:05 +0700

    Update Test Case

--- MyFunction.py
+++ MyFunction.py
@@ -10,20 +10,23 @@ import pytest
 import allure
 
 
-def Search_Website(self,start_title):
+def Search_Website(self, start_title):
     driver = self.driver
     driver.get(self.start_url)
     self.assertIn(start_title, driver.title)
     return driver
 
+
 def Visit_Login_Page(self):
-    driver=Search_Website(self,'Reales')
+    driver = Search_Website(self, 'Reales')
     driver.find_element(By.LINK_TEXT, "Đăng nhập").click()
     url = driver.current_url
-    self.assertEqual(self.start_url+'login',url)
+    self.assertEqual(self.start_url+'login', url)
     return driver
+
+
 def Login_Google(self):
-    driver=Visit_Login_Page(self)
+    driver = Visit_Login_Page(self)
     with allure.step('Login With Google Account'):
         GGButton = driver.find_element_by_name("googleLoginButton")
         time.sleep(1)
@@ -39,4 +42,19 @@ def Login_Google(self):
         time.sleep(6)
         driver.switch_to.window(driver.window_handles[0])
         self.assertTrue(driver.find_elements_by_name("accountDetail"))
+        return driver
+
+
+def Login_Admin(self):
+    driver = Search_Website(self, 'Admin RealState')
+    with allure.step('Input Email and Pass'):
+        inputLogin = driver.find_element_by_name("email")
+        inputLogin.send_keys('trantandat130497@gmail.com')
+        inputPass = driver.find_element_by_name("password")
+        inputPass.send_keys('12345')
+    with allure.step('Click Button Đăng Nhập'):
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/form/div[3]/button').click()
+        time.sleep(2)
+        url = driver.current_url
+        self.assertEqual(url, 'https://admin.myreales.tk/')
         return driver
--- TestAdmin.py
+++ TestAdmin.py
@@ -1,7 +1,8 @@
 import time
 import os
+import random
 import unittest
-from MyFunction import Search_Website
+from MyFunction import Search_Website, Login_Admin
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -21,6 +22,7 @@ admin_images = current_url+'/images/Admin'
 if not os.path.exists(admin_images):
     os.mkdir(admin_images)
 
+
 class TestAdmin(unittest.TestCase):
 
     def setUp(self):
@@ -31,7 +33,7 @@ class TestAdmin(unittest.TestCase):
     def test_01_search_in_browser(self):
         driver = Search_Website(self, 'Admin RealState')
         driver.save_screenshot(admin_images+"/test_01_search_in_browser.png")
-    
+
     def test_02a_admin_login_without_user_and_pass(self):
         driver = Search_Website(self, 'Admin RealState')
         with allure.step('Click Button Đăng nhập'):
@@ -78,7 +80,6 @@ class TestAdmin(unittest.TestCase):
             self.assertFalse("Email is required" in bodyText)
             self.assertTrue("Password is required" in bodyText)
             driver.save_screenshot(admin_images+"/test_02d_admin_login_without_pass_and_right_format_email.png")
-    
 
     def test_02e_admin_login_failed(self):
         driver = Search_Website(self, 'Admin RealState')
@@ -88,7 +89,7 @@ class TestAdmin(unittest.TestCase):
             inputLogin.send_keys('abc@gmail.com')
             inputPass=driver.find_element_by_name("password")
             inputPass.send_keys('123')
-        driver.save_screenshot(admin_images+"/test_02e_admin_login_failed.png")    
+        driver.save_screenshot(admin_images+"/test_02e_admin_login_failed.png")
         with allure.step('Click Button Đăng Nhập'):
             driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/form/div[3]/button').click()
             time.sleep(3)
@@ -103,12 +104,184 @@ class TestAdmin(unittest.TestCase):
             inputPass=driver.find_element_by_name("password")
             inputPass.send_keys('12345')
             driver.save_screenshot(admin_images+"/test_02f_admin_login_fill.png")
+        with allure.step('Click Button Đăng Nhập'):
             driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/form/div[3]/button').click()
             time.sleep(2)
             url = driver.current_url
             self.assertEqual(url,'https://admin.myreales.tk/')
             driver.save_screenshot(admin_images+"/test_02f_admin_login_success.png")
+
+    def test_03a_visit_account_page(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(2)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+            driver.save_screenshot(admin_images+"/test_03a_visit_account_page.png")
+
+    def test_03b_visit_project_page(self):
+        driver = Login_Admin(self)
+        with allure.step('Click Dự án'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[3]/a').click()
+            time.sleep(2)
+            self.assertEqual(driver.current_url, 'https://admin.myreales.tk/project/1')
+            driver.save_screenshot(admin_images+"/test_03b_visit_project_page.png")
+
+    def test_03c_visit_news_page(self):
+        driver = Login_Admin(self)
+        with allure.step('Click Bài Viết'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[4]/a').click()
+            time.sleep(2)
+            self.assertEqual(driver.current_url, 'https://admin.myreales.tk/news/1')
+            driver.save_screenshot(admin_images+"/test_03c_visit_news_page.png")
+
+    def test_03d_visit_company_page(self):
+        driver = Login_Admin(self)
+        with allure.step('Click Công ty'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[5]/a').click()
+            time.sleep(2)
+            self.assertEqual(driver.current_url, 'https://admin.myreales.tk/company/1')
+            driver.save_screenshot(admin_images+"/test_03d_visit_company_page.png")
+
+    def test_04a_see_account_info(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(1)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+        with allure.step('Click Random Account'):
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div/div/div/div/div/table/tbody/tr[3]').click()
+            time.sleep(1)
+            bodyText = driver.find_element_by_tag_name('body').text
+            self.assertTrue("Thông tin chi tiết tài khoản" in bodyText)
+            driver.save_screenshot(admin_images+"/test_04a_see_account_info.png")
+
+    def test_04b_update_name_account_info(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(1)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+        with allure.step('Click Random Account'):
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div/div/div/div/div/table/tbody/tr[3]').click()
+            time.sleep(1)
+            bodyText = driver.find_element_by_tag_name('body').text
+            self.assertTrue("Thông tin chi tiết tài khoản" in bodyText)
+        with allure.step('Update name'):
+            driver.find_element_by_xpath('//*[@id="fullname"]').clear()
+            i=random.randint(1,10)
+            driver.find_element_by_xpath('//*[@id="fullname"]').send_keys('Tester',i)
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div[1]/div[2]/form/div[4]/div[1]/button').click()
+            time.sleep(3)
+            test=driver.find_elements_by_class_name("ant-message")
+            text=test[0].text
+            self.assertEqual(text,'Update Done')
+            driver.save_screenshot(admin_images+"/test_04b_update_name_account_info.png")
+
+    def test_04b_update_phone_account_info(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(1)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+        with allure.step('Click Random Account'):
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div/div/div/div/div/table/tbody/tr[3]').click()
+            time.sleep(1)
+            bodyText = driver.find_element_by_tag_name('body').text
+            self.assertTrue("Thông tin chi tiết tài khoản" in bodyText)
+        with allure.step('Update Phone'):
+            driver.find_element_by_xpath('//*[@id="phone"]').clear()
+            driver.find_element_by_xpath('//*[@id="phone"]').send_keys('0903005908')
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div[1]/div[2]/form/div[4]/div[1]/button').click()
+            time.sleep(3)
+            test=driver.find_elements_by_class_name("ant-message")
+            text=test[0].text
+            self.assertEqual(text,'Update Done')
+            driver.save_screenshot(admin_images+"/test_04b_update_phone_account_info.png")
+
+    def test_04c_update_address_account_info(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(1)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+        with allure.step('Click Random Account'):
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div/div/div/div/div/table/tbody/tr[3]').click()
+            time.sleep(1)
+            bodyText = driver.find_element_by_tag_name('body').text
+            self.assertTrue("Thông tin chi tiết tài khoản" in bodyText)
+        with allure.step('Update address'):
+            i=random.randint(1,10)
+            driver.find_element_by_xpath('//*[@id="address"]').clear()
+            driver.find_element_by_xpath('//*[@id="address"]').send_keys('Ho Chi Minh',i)
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div[1]/div[2]/form/div[4]/div[1]/button').click()
+            time.sleep(3)
+            test=driver.find_elements_by_class_name("ant-message")
+            text=test[0].text
+            self.assertEqual(text,'Update Done')
+            driver.save_screenshot(admin_images+"/test_04c_update_phone_account_info.png")
     
+    def test_04d_update_description_account_info(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(1)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+        with allure.step('Click Random Account'):
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div/div/div/div/div/table/tbody/tr[3]').click()
+            time.sleep(1)
+            bodyText = driver.find_element_by_tag_name('body').text
+            self.assertTrue("Thông tin chi tiết tài khoản" in bodyText)
+        with allure.step('Update description'):
+            i=random.randint(1,10)
+            driver.find_element_by_xpath('//*[@id="description"]').clear()
+            driver.find_element_by_xpath('//*[@id="description"]').send_keys('I am a tester',i)
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div[1]/div[2]/form/div[4]/div[1]/button').click()
+            time.sleep(3)
+            test=driver.find_elements_by_class_name("ant-message")
+            text=test[0].text
+            self.assertEqual(text,'Update Done')
+            driver.save_screenshot(admin_images+"/test_04d_update_description_account_info.png")
+
+    def test_04e_update_status_account_info(self):
+        driver = Login_Admin(self)
+        with allure.step('Click  Tài Khoản'):
+            driver.find_element_by_xpath(
+                '//*[@id="wrapper"]/div[1]/ul/li[2]/a').click()
+            time.sleep(1)
+            self.assertEqual(driver.current_url,
+                            'https://admin.myreales.tk/account/1')
+        with allure.step('Click Random Account'):
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div/div/div/div/div/table/tbody/tr[3]').click()
+            time.sleep(1)
+            bodyText = driver.find_element_by_tag_name('body').text
+            self.assertTrue("Thông tin chi tiết tài khoản" in bodyText)
+        with allure.step('Update status'):
+            statusAccount=Select(driver.find_element_by_id('statusAccount'))
+            statusAccount.select_by_index(1)
+            driver.find_element_by_xpath('//*[@id="content-wrapper"]/div/div[2]/div[2]/div[1]/div[2]/form/div[4]/div[1]/button').click()
+            time.sleep(3)
+            test=driver.find_elements_by_class_name("ant-message")
+            text=test[0].text
+            self.assertEqual(text,'Update Done')
+            driver.save_screenshot(admin_images+"/test_04e_update_status_account_info.png")
+          
+
     def tearDown(self):
         # self.driver.close()
         self.driver.quit()
